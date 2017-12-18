792667d63d1814a27a5a8c2a48b2e27ca2c22ab4
JackMatusiewicz <jack.matusiewicz@gmail.com>
2017-12-18 23:50:44 +0000

    Finishing off rough prototype - moving things over to bounded floats rather than bigints.

--- ShamirsSecretSharing/Program.fs
+++ ShamirsSecretSharing/Program.fs
@@ -1,72 +1,108 @@
 ﻿open System
 open System.Numerics
 
+module Reader =
+
+    let map (f : 'a -> 'b) (x : 'r -> 'a) : 'r -> 'b =
+        x >> f
+    let (<!>) = map
+
+    let apply (f : 'r -> 'a -> 'b) (x : 'r -> 'a) : 'r -> 'b =
+        fun r -> f r (x r)
+    let (<*>) = apply
+
+open Reader
+
+type Share = int * float
+
 //TODO - in order to actually be useful, this must use finite field arithmetic!
 
-type Secret = {
-    Value : bigint
-    Threshold : uint32
-}
+type ShareGenerator =
+    abstract member GenerateSecret : uint32*uint32*int -> Share list
 
-type PolynomialTerm = {
-    Power : int
-    Coefficient : bigint
-}
+module SecretDistribution =
 
-type SecretGraph = { //TODO - make private
-    PolynomialTerms : PolynomialTerm list
-}
+    type Secret = {
+        Value : int
+        Threshold : uint32
+    }
 
-type Share = {
-    ShareNumber : int //x
-    Share : bigint //y
-}
+    type PolynomialTerm = {
+        Power : int
+        Coefficient : int
+    }
 
-let createSecretGraph (threshold : uint32) (secret : bigint) : SecretGraph =
-    let rec create (thresh : uint32) (acc : PolynomialTerm list) =
-        match thresh with
-        | 0u ->
-            let constant = {Power = 0; Coefficient = secret}
-            (constant :: acc) |> List.rev
-        | _ ->
-            let term = {Power = (int thresh); Coefficient = (bigint 5)} //TODO - replace this with random generation
-            create (thresh - 1u) (term :: acc)
-    let terms = create (threshold - 1u) []
-    {PolynomialTerms = terms}
+    type SecretGraph = { //TODO - make private
+        PolynomialTerms : PolynomialTerm list
+    }
 
-let calculateShare (shareNumber : int) (graph : SecretGraph) : Share =
-    let bigIntShare = (bigint shareNumber)
-    graph.PolynomialTerms
-    |> List.map (fun term -> BigInteger.Pow(bigIntShare, term.Power) * term.Coefficient)
-    |> List.fold (+) (bigint 0)
-    |> (fun share -> {ShareNumber = shareNumber; Share = share})
+    let private createSecretGraph (threshold : uint32) (secret : int) : SecretGraph =
+        let rec create (thresh : uint32) (acc : PolynomialTerm list) =
+            match thresh with
+            | 0u ->
+                let constant = {Power = 0; Coefficient = secret}
+                (constant :: acc) |> List.rev
+            | _ ->
+                let term = {Power = (int thresh); Coefficient = 5} //TODO - replace this with random generation
+                create (thresh - 1u) (term :: acc)
+        let terms = create (threshold - 1u) []
+        {PolynomialTerms = terms}
 
-//This is the Share Computation side of SSS, once this is done, the graph can be destroyed.
-let createDesiredShares (numberOfShares : int) (graph : SecretGraph) =
-    let r = System.Random() //TODO - replace with real source of randomness.
-    let rec create (remaining : int) (acc : Share list) =
-        match remaining with
-        | _ when remaining <= 0 -> 
-            acc
-        | _ ->
-            let nextShare = r.Next()
-            let next = calculateShare nextShare graph
-            create (remaining - 1) (next :: acc)
-    create numberOfShares []  
+    let private calculateShare (shareNumber : int) (graph : SecretGraph) : Share =
+        let floatShare = (float shareNumber)
+        graph.PolynomialTerms
+        |> List.map (fun term -> Math.Pow(floatShare, (float term.Power)) * (float term.Coefficient))
+        |> List.fold (+) 0.0
+        |> (fun share -> (shareNumber, share))
 
-//This is the secret reconstruction part of SSS. Using the threshold of the secret and the number of shares we have,
-//we can reconstruct the graph.
-let constructGraph (shares : Share list) (threshold : uint32) =
-    if (shares |> List.length |> uint32) < threshold then
-        failwithf "Need more than %d shares to compute secret" threshold
-    else
-        failwith "TODO"           
+    //This is the Share Computation side of SSS, once this is done, the graph can be destroyed.
+    let private createDesiredShares (numberOfShares : int) (graph : SecretGraph) =
+        let r = System.Random() //TODO - replace with real source of randomness.
+        let rec create (remaining : int) (acc : Share list) =
+            match remaining with
+            | _ when remaining <= 0 -> 
+                acc
+            | _ ->
+                let nextShare = r.Next(5000) //TODO - replace with BigNum so we can have arbitrarily large numbers
+                let next = calculateShare nextShare graph
+                create (remaining - 1) (next :: acc)
+        create numberOfShares []
+
+    let make () =
+        { new ShareGenerator with
+                member __.GenerateSecret (thresh, shares, secret) =
+                    createSecretGraph thresh secret
+                    |> createDesiredShares (int shares) }
+
+module SecretReconstruction =
+
+    let private interpolate (vals : Share list) ((thisX,_) : Share) : int -> float =
+        vals
+        |> List.filter (fun (x,_) -> x <> thisX)
+        |> List.map (fun (x,_) -> fun z -> (float (z - x)) / (float (thisX - x)))
+        |> List.fold (fun f g -> (*) <!> f <*> g) (fun _ -> 1.0)
+
+    let private constructPolynomial (vals : Share list) : int -> float =
+        vals
+        |> List.map (interpolate vals)
+        |> List.zip vals
+        |> List.map (fun ((_,y), f) -> fun x -> y * (f x))
+        |> List.fold (fun f g -> (+) <!> f <*> g) (fun _ -> 0.0)
+
+    let getSecret (threshold : uint32) (shares : Share list) : float =
+        if (shares |> List.length |> uint32) < threshold then
+            failwithf "Need more than %d shares to compute secret" threshold
+        else
+            constructPolynomial shares 0
+            
+            
+let test () =  
+    let mySecret = 11234
+    let generator = SecretDistribution.make()
+    let shares = generator.GenerateSecret (3u, 6u, mySecret)
+    let secret = SecretReconstruction.getSecret 3u shares
+    printfn "%f" secret
 
 [<EntryPoint>]
-let main argv = 
-    let mySecret = (bigint 1303)
-    let graph = createSecretGraph 2u mySecret
-    let share = createDesiredShares 3 graph
-    printfn "%A" share
-    printfn "%A" graph
-    0 // return an integer exit code
+let main argv =
+    0
