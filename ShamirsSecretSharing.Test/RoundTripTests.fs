7557b492111d0e016c104d7016c5782b8b7489ac
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2017-12-27 12:08:20 +0000

    Adding test project with some basic round trip tests

---
+++ ShamirsSecretSharing.Test/RoundTripTests.fs
@@ -0,0 +1,20 @@
+﻿namespace ShamirsSecretSharing.Test
+
+open NUnit.Framework
+open SecretSharing
+
+module RoundTripTests =
+
+    let ``Given a secret and a required number of shares, when those shares are present then secret is returned`` () =
+            let mySecret = 11234
+            let generator = SecretSharing.makeGenerator()
+            let shares = generator.GenerateSecret (3u, 6u, mySecret) |> List.take 3
+            let secret = SecretSharing.getSecret 3u shares
+            Assert.That (secret, Is.EqualTo(mySecret))
+
+    let ``Given a secret and a required number of shares, when not enough shares are present then secret is not returned`` () =
+            let mySecret = 11234
+            let generator = SecretSharing.makeGenerator()
+            let shares = generator.GenerateSecret (3u, 6u, mySecret) |> List.take 2
+            let secret = SecretSharing.getSecret 2u shares
+            Assert.That (secret, Is.Not.EqualTo(mySecret))
