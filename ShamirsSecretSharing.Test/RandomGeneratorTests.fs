a071b567f34fffd8031669e2db620c7db58f27f5
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2017-12-27 17:12:55 +0000

    Adding random generators and tests

---
+++ ShamirsSecretSharing.Test/RandomGeneratorTests.fs
@@ -0,0 +1,32 @@
+namespace ShamirsSecretSharing.Test
+
+open NUnit.Framework
+open SecretSharing
+open System.Collections.Generic
+
+[<TestFixture>]
+module RandomGeneratorTests =
+
+    [<Test>]
+    let ``Given a random int generator, when asked to generate 100 numbers then all are unique`` () =
+        let generator = RandomGeneration.makeRandomIntGenerator ()
+        let values = Array.init 100 (fun _ -> RandomGeneration.generate generator)
+        let store = HashSet<int>()
+        Array.fold (fun (store : HashSet<int>) v ->
+                        if store.Contains(v) then
+                            Assert.Fail ()
+                            store
+                        else
+                            store.Add(v) |> ignore; store) store values |> ignore
+
+    [<Test>]
+    let ``Given an int generator, when ints are generated then all are greater than or equal to 0`` () =
+        let generator = RandomGeneration.makeRandomIntGenerator ()
+        Array.init 100 (fun _ -> RandomGeneration.generate generator)
+        |> Array.iter (fun i -> if i < 0 then Assert.Fail())
+
+    [<Test>]
+    let ``Given an bigint generator, when ints are generated then all are greater than or equal to 0`` () =
+        let generator = RandomGeneration.makeRandomBigintGenerator 10
+        Array.init 100 (fun _ -> RandomGeneration.generate generator)
+        |> Array.iter (fun i -> if i < (bigint 0) then Assert.Fail())
--- ShamirsSecretSharing/RandomGeneration.fs
+++ ShamirsSecretSharing/RandomGeneration.fs
@@ -1,10 +1,33 @@
 namespace SecretSharing
 
+open System
+open System.Numerics
+open System.Security.Cryptography
+
 type RandomGeneration<'t> =
-    | Generator of (unit -> 't)
+    | Generator of (unit -> 't) * IDisposable
 
 module RandomGeneration =
 
     let generate (generator : RandomGeneration<'t>) : 't =
-        let (Generator rg) = generator
+        let (Generator (rg,_)) = generator
         rg ()
+
+    ///Generates a random positive integer
+    let makeRandomIntGenerator () =
+        let r = new RNGCryptoServiceProvider()
+        let f = fun () ->
+                    let store = Array.create 4 (byte 0)
+                    r.GetBytes(store)
+                    let value = BitConverter.ToInt32(store,0)
+                    value &&& (0x7FFFFFFF)
+        Generator (f, r :> IDisposable)
+
+    ///Generates a random positive big integer, of the required size.
+    let makeRandomBigintGenerator (bytesInBigint : int) =
+        let r = new RNGCryptoServiceProvider()
+        let f = fun () ->
+                    let store = Array.create bytesInBigint (byte 0)
+                    store.[bytesInBigint - 1] <- (store.[bytesInBigint - 1] &&& (byte 0x7F))
+                    BigInteger(store)
+        Generator (f,r)
