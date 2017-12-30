66cb9b49f28eb71784fc06ea53c8c3e452ad38dc
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2017-12-30 08:40:42 +0000

    Adding math functions to enable FFA division

---
+++ ShamirsSecretSharing.Test/MathTests.fs
@@ -0,0 +1,49 @@
+namespace SecretSharing.Test
+
+open NUnit.Framework
+open SecretSharing
+open System.Numerics
+open Math
+
+module MathTests =
+
+    let gcd (a : bigint) (b : bigint) =
+        BigInteger.GreatestCommonDivisor(a,b)
+
+    let numGenerator = RandomGeneration.makeRandomBigintGenerator 3
+    let prime = (bigint 65537)
+
+    [<Test>]
+    let ``Given random modulus and number, when calculating modular inverse then result is correct``() =
+        for i in 1 .. 200 do
+            let a = RandomGeneration.generate numGenerator
+            let modInv = Math.modularInverse a prime
+            match modInv with
+            | Some mi ->
+                Assert.That((a * mi) % prime, Is.EqualTo(bigint 1))
+            | None -> Assert.Fail("Two primes should have an inverse")
+
+    [<Test>]
+    [<Repeat(50)>]
+    let ``Given two numbers, m and n, the gcd multiplied by lcm equals m * n``() =
+        let makeTuple a b = a,b
+        let a,b = (RandomGeneration.generate numGenerator),(RandomGeneration.generate numGenerator)
+
+        Assert.That((gcd a b) * (lcm a b), Is.EqualTo(a * b))
+
+    [<Test>]
+    [<Repeat(50)>]
+    let ``Given two numbers, m and n, if there is a common factor then modInverse returns none``() =
+        let a = (RandomGeneration.generate numGenerator)
+        let b = (RandomGeneration.generate numGenerator)
+
+        Assert.That(Math.modularInverse a (a * b), Is.EqualTo(None))
+
+    [<Test>]
+    [<Repeat(30)>]
+    let ``Given three numbers, a,b,c then lcm (a.b, a.c) = a . lcm(b,c)``() =
+        let a = (RandomGeneration.generate numGenerator)
+        let b = (RandomGeneration.generate numGenerator)
+        let c = (RandomGeneration.generate numGenerator)
+
+        Assert.That(lcm (a*b) (a*c), Is.EqualTo(a * (lcm b c)))
---
+++ ShamirsSecretSharing/Math.fs
@@ -0,0 +1,30 @@
+namespace SecretSharing
+
+open System.Numerics
+
+module Math =
+
+    let lcm (a : BigInteger) (b : BigInteger) =
+        if a = (bigint 0) && b = (bigint 0) then
+            (bigint 0)
+        else
+            let gcd = BigInteger.GreatestCommonDivisor(a,b)
+            (a * b) / gcd
+
+    let rec extendedGcd (a : bigint) (b : bigint) =
+        match a,b with
+        | (a,b) when b = (bigint 0) -> (bigint 1, bigint 0, a)
+        | _ ->
+            let quotient = BigInteger.Divide(a,b)
+            let rem = BigInteger.Remainder(a,b)
+            let (x,y,z) = extendedGcd b rem
+            (y, x - quotient * y, z)
+
+    let modularInverse (a : bigint) (modulus : bigint) =
+        let (i, _, k) = extendedGcd a modulus
+        if k = (bigint 1) then
+            if i < (bigint 0) then
+                Some <| i + modulus
+            else
+                Some i
+        else None
