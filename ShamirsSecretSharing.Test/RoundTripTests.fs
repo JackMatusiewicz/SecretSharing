a2ace5e1f10ef77cd4c7d26d54bdcd022ac6b5b0
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2017-12-30 09:12:15 +0000

    Making round trip tests slightly better: now have random input and repeat multiple times

--- ShamirsSecretSharing.Test/RoundTripTests.fs
+++ ShamirsSecretSharing.Test/RoundTripTests.fs
@@ -6,25 +6,32 @@ open SecretSharing
 [<TestFixture>]
 module RoundTripTests =
 
-    [<Test>]
-    let ``Given a secret and a required number of shares, when those shares are present then secret is returned`` () =
-            let mySecret = bigint 11234
-            let generator = SecretSharing.makeGenerator()
-            let shares = generator.GenerateSecret (3u, 6u, mySecret) |> List.take 3
-            let secret = SecretSharing.getSecret 3u shares
-            Assert.That (secret, Is.EqualTo(mySecret))
-    
-    [<Test>]
-    let ``Given a secret and a required number of shares, when not enough shares are present then error is thrown`` () =
-            let mySecret = bigint 11234
-            let generator = SecretSharing.makeGenerator()
-            let shares = generator.GenerateSecret (3u, 6u, mySecret) |> List.take 2
-            Assert.Throws<System.Exception>(fun () -> SecretSharing.getSecret 3u shares |> ignore) |> ignore
 
     [<Test>]
+    [<Repeat(30)>]
+    let ``Given a secret and a required number of shares, when those shares are present then secret is returned`` () =
+        let gen = RandomGeneration.makeRandomBigintGenerator 4
+        let mySecret = RandomGeneration.generate gen
+        let generator = SecretSharing.makeGenerator()
+        let shares = generator.GenerateSecret (3u, 6u, mySecret) |> List.take 3
+        let secret = SecretSharing.getSecret 3u shares
+        Assert.That (secret, Is.EqualTo(mySecret))
+    
+    [<Test>]
+    [<Repeat(30)>]
+    let ``Given a secret and a required number of shares, when not enough shares are present then error is thrown`` () =
+        let gen = RandomGeneration.makeRandomBigintGenerator 4
+        let mySecret = RandomGeneration.generate gen
+        let generator = SecretSharing.makeGenerator()
+        let shares = generator.GenerateSecret (3u, 6u, mySecret) |> List.take 2
+        Assert.Throws<System.Exception>(fun () -> SecretSharing.getSecret 3u shares |> ignore) |> ignore
+
+    [<Test>]
+    [<Repeat(30)>]
     let ``Given a secret and a required number of shares, when not enough shares are present then secret is not returned`` () =
-            let mySecret = bigint 10848292
-            let generator = SecretSharing.makeGenerator()
-            let shares = generator.GenerateSecret (5u, 6u, mySecret) |> List.take 1
-            let secret = SecretSharing.getSecret 1u shares
-            Assert.That (secret, Is.Not.EqualTo(mySecret))
+        let gen = RandomGeneration.makeRandomBigintGenerator 4
+        let mySecret = RandomGeneration.generate gen
+        let generator = SecretSharing.makeGenerator()
+        let shares = generator.GenerateSecret (5u, 6u, mySecret) |> List.take 1
+        let secret = SecretSharing.getSecret 1u shares
+        Assert.That (secret, Is.Not.EqualTo(mySecret))
