00169138900d714880ceeed3258932971e29e4d8
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2017-12-31 11:09:00 +0000

    Updating tests
    Upping number of repeats

--- ShamirsSecretSharing.Test/RoundTripTests.fs
+++ ShamirsSecretSharing.Test/RoundTripTests.fs
@@ -8,7 +8,7 @@ module RoundTripTests =
 
 
     [<Test>]
-    [<Repeat(30)>]
+    [<Repeat(300)>]
     let ``Given a secret and a required number of shares, when those shares are present then secret is returned`` () =
         let gen = RandomGeneration.makeRandomBigintGenerator 4
         let mySecret = RandomGeneration.generate gen
@@ -18,7 +18,7 @@ module RoundTripTests =
         Assert.That (secret, Is.EqualTo(mySecret))
     
     [<Test>]
-    [<Repeat(30)>]
+    [<Repeat(300)>]
     let ``Given a secret and a required number of shares, when not enough shares are present then error is thrown`` () =
         let gen = RandomGeneration.makeRandomBigintGenerator 4
         let mySecret = RandomGeneration.generate gen
@@ -27,7 +27,7 @@ module RoundTripTests =
         Assert.Throws<System.Exception>(fun () -> SecretSharing.getSecret 3u shares |> ignore) |> ignore
 
     [<Test>]
-    [<Repeat(30)>]
+    [<Repeat(300)>]
     let ``Given a secret and a required number of shares, when not enough shares are present then secret is not returned`` () =
         let gen = RandomGeneration.makeRandomBigintGenerator 4
         let mySecret = RandomGeneration.generate gen
