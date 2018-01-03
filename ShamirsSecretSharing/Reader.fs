d3693daba1bde495572f6dd22161c088e84348ad
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2018-01-03 11:14:19 +0000

    Removing older reader map operator

--- ShamirsSecretSharing/Reader.fs
+++ ShamirsSecretSharing/Reader.fs
@@ -4,7 +4,6 @@ module Reader =
 
     let map (f : 'a -> 'b) (x : 'r -> 'a) : 'r -> 'b =
         x >> f
-    let (<!>) = map
     let (<?>) x f = map f x
 
     let apply (f : 'r -> 'a -> 'b) (x : 'r -> 'a) : 'r -> 'b =
