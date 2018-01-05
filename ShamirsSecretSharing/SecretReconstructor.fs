33529c527b29f163e0379b0b80284648a36367b4
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-01-05 17:08:25 +0000

    Moving modules into correctly named files

--- ShamirsSecretSharing/SecretReconstructor.fs
+++ ShamirsSecretSharing/SecretReconstructor.fs
@@ -5,41 +5,8 @@ open System
 open System.Numerics
 open Math
 
-type Coordinate = bigint * bigint
-
-type Prime = bigint
-
-type CoordinateGenerator =
-    abstract member GenerateCoordinates : uint32*uint32*bigint -> Prime * Coordinate list
-
 type SecretReconstructor =
     abstract member ReconstructSecret : bigint*Coordinate list -> bigint
-
-module SecretSharer =
-
-    let private createCoordinates
-        (numberOfCoordinates : int)
-        (rg : RandomGenerator<bigint>)
-        (polynomial : Polynomial) =
-        let rec create (remaining : int) (acc : Coordinate list) =
-            match remaining with
-            | _ when remaining <= 0 ->
-                acc
-            | _ ->
-                let x = RandomGenerator.generate rg
-                let y = Polynomial.evaluate x polynomial
-                create (remaining - 1) ((x,y) :: acc)
-        create numberOfCoordinates []
-
-    let make () =
-        { new CoordinateGenerator with
-                member __.GenerateCoordinates (minimumSegementsToSolve, numberOfCoords, secret) =
-                    let prime = BigInt.findLargerMersennePrime secret
-                    let generator = RandomGenerator.makeRandomBigIntRange prime
-                    let poly = Polynomial.create minimumSegementsToSolve generator secret prime
-                    poly
-                    |> createCoordinates (int numberOfCoords) generator
-                    |> (fun shares -> poly.Prime,shares) }
 
 module SecretReconstructor =
 
