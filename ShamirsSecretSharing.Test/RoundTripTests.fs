6b6294035bf23c3a659384e5612a8ddac243793e
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-01-05 17:47:02 +0000

    Changing how the roundtrip test works

--- ShamirsSecretSharing.Test/RoundTripTests.fs
+++ ShamirsSecretSharing.Test/RoundTripTests.fs
@@ -7,16 +7,17 @@ open SecretSharing
 module RoundTripTests =
 
     [<Test>]
-    [<Repeat(2000)>]
+    //[<Repeat(2000)>]
     let ``Given a secret and a required number of shares, when those shares are present then secret is returned`` () =
-        let gen = RandomGenerator.makeRandomBigintGenerator 40
-        let mySecret = RandomGenerator.generate gen
-        let generator = SecretSharer.make()
-        let p, shares = generator.GenerateCoordinates (3u, 6u, mySecret)
-        let shares = shares |> List.take 3
-        let recon = SecretReconstructor.make ()
-        let secret = recon.ReconstructSecret (p,shares)
-        Assert.That (secret, Is.EqualTo(mySecret))
+        for i in 0 .. 2000 do
+            let gen = RandomGenerator.makeRandomBigintGenerator 40
+            let mySecret = RandomGenerator.generate gen
+            let generator = SecretSharer.make()
+            let p, shares = generator.GenerateCoordinates (3u, 6u, mySecret)
+            let shares = shares |> List.take 3
+            let recon = SecretReconstructor.make ()
+            let secret = recon.ReconstructSecret (p,shares)
+            Assert.That (secret, Is.EqualTo(mySecret))
     
     [<Test>]
     [<Repeat(300)>]
