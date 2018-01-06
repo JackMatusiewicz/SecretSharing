8bd9ed5888b0494dd76ebaee5e1c5ea41fef4123
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-01-06 22:25:15 +0000

    Adding custom sharer and reconstructor

---
+++ ShamirsSecretSharing/CustomReconstructor.fs
@@ -0,0 +1,20 @@
+﻿namespace SecretSharing
+
+open System
+open System.Numerics
+
+type ICustomReconstructor<'a> =
+    abstract member ReconstructSecret : bigint * Coordinate list -> 'a
+
+//Wraps the SecretReconstructor so you can deal with anything, rather than with bigints.
+module CustomReconstructor =
+
+    let getPassword (convert : bigint -> 'a) (prime : bigint) (coords : Coordinate list) =
+        SecretReconstructor.getSecret prime coords
+        |> convert
+
+    let make (convert : Func<bigint, 'a>) =
+        { new IPasswordReconstructor with
+                member __.ReconstructSecret (prime, coords) : string =
+                    let f = Function.fromFunc convert
+                    getPassword f prime coords }
---
+++ ShamirsSecretSharing/Function.fs
@@ -0,0 +1,8 @@
+﻿namespace SecretSharing
+
+open System
+
+module Function =
+
+    let fromFunc (f : Func<'a, 'b>) : 'a -> 'b =
+        fun a -> f.Invoke(a)
