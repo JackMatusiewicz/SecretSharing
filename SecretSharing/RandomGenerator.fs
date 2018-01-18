73dbd4cc93c2ccf148a189169849de5f3a474025
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-01-18 00:23:23 +0000

    Changing formatting of lambdas

--- SecretSharing/RandomGenerator.fs
+++ SecretSharing/RandomGenerator.fs
@@ -17,21 +17,23 @@ module RandomGenerator =
     ///Generates a random positive integer
     let makeRandomIntGenerator () =
         let r = new RNGCryptoServiceProvider()
-        let f = fun () ->
-                    let store = Array.create 4 (byte 0)
-                    r.GetBytes(store)
-                    let value = BitConverter.ToInt32(store,0)
-                    (value &&& (0x7FFFFFFF))
+        let f =
+            fun () ->
+                let store = Array.create 4 (byte 0)
+                r.GetBytes(store)
+                let value = BitConverter.ToInt32(store,0)
+                (value &&& (0x7FFFFFFF))
         Generator f
 
     ///Generates a random positive big integer, of the required size.
     let makeRandomBigintGenerator (bytesInBigint : int) =
         let r = new RNGCryptoServiceProvider()
-        let f = fun () ->
-                    let store = Array.create bytesInBigint (byte 0)
-                    r.GetBytes(store)
-                    store.[bytesInBigint - 1] <- (store.[bytesInBigint - 1] &&& (byte 0x7F))
-                    BigInteger(store)
+        let f =
+            fun () ->
+                let store = Array.create bytesInBigint (byte 0)
+                r.GetBytes(store)
+                store.[bytesInBigint - 1] <- (store.[bytesInBigint - 1] &&& (byte 0x7F))
+                BigInteger(store)
         Generator f
 
     ///This picks in the range of 0, (max - 1), will throw if max is negative.
