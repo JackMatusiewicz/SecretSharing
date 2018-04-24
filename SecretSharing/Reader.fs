cb3c8b966e41b9d98658d7ae8e861a8d50241eb0
JackMatusiewicz <jack.matusiewicz@gmail.com>
2018-04-24 20:48:22 +0100

    removing function composition to avoid callvirt

--- SecretSharing/Reader.fs
+++ SecretSharing/Reader.fs
@@ -3,7 +3,7 @@ namespace SecretSharing
 module Reader =
 
     let map (f : 'a -> 'b) (x : 'r -> 'a) : 'r -> 'b =
-        x >> f
+        fun a -> a |> x |> f
     let (<?>) x f = map f x
 
     let apply (f : 'r -> 'a -> 'b) (x : 'r -> 'a) : 'r -> 'b =
