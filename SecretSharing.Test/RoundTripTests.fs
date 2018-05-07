ccd78889f440c6cbd00e6090104c972d761531ae
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-05-07 12:30:35 +0100

    Adding test to confirm that reconstruction works on any platform test runs on

--- SecretSharing.Test/RoundTripTests.fs
+++ SecretSharing.Test/RoundTripTests.fs
@@ -46,7 +46,7 @@ module RoundTripTests =
     let ``Given a secret and a required number of shares, when not enough shares are present then secret is not returned`` () =
         let gen = RandomGenerator.makeRandomBigintGenerator 4
         let mySecret = RandomGenerator.generate gen
-        let generator = SecretSharer.make()
+        let generator = SecretSharer.make ()
         let ret = generator.GenerateCoordinates (5u, 6u, mySecret)
         let allSharePermutations = Permutations.ofLength 1 (List.ofSeq ret.Shares)
         let shares = List.map (Function.toGenericList) allSharePermutations
@@ -54,3 +54,13 @@ module RoundTripTests =
         for share in shares do
             let secret = recon.ReconstructSecret (ret.Prime,share)
             Assert.That (secret, Is.Not.EqualTo(mySecret))
+
+    [<Test>]
+    let ``Given shares, when reconstructed, then secret is correct`` () =
+        let secret = (bigint 5538956)
+        let prime = (bigint 2147483647)
+        let c1 = {X = (bigint 198773898); Y = (bigint 1215122073)}
+        let c2 = {X = (bigint 511737190); Y = (bigint 1566900739)}
+        let reconstructor = SecretReconstructor.make ()
+        let result = reconstructor.ReconstructSecret (prime, Function.toGenericList [c1;c2])
+        Assert.That(result, Is.EqualTo(secret))
