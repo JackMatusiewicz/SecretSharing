9d3f39e26c88b61029c074e75525c4fbf8a0f3fb
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2018-05-16 07:28:29 +0100

    Fixing indentation

--- SecretSharing.Test/ThresholdSchemeTests.fs
+++ SecretSharing.Test/ThresholdSchemeTests.fs
@@ -10,13 +10,13 @@ open Function
 module ThresholdSchemeTests =
 
     [<Test>]
-        let ``Given valid values, when creating a threshold scheme, then scheme is returned`` () =
-            let a = 7u
-            let b = 9u
+    let ``Given valid values, when creating a threshold scheme, then scheme is returned`` () =
+        let a = 7u
+        let b = 9u
 
-            let scheme = ThresholdScheme.make (b, a)
-            Assert.That(ThresholdScheme.numberOfSharesForRecovery scheme, Is.EqualTo(7u))
-            Assert.That(ThresholdScheme.numberOfSharesToMake scheme, Is.EqualTo(9u))
+        let scheme = ThresholdScheme.make (b, a)
+        Assert.That(ThresholdScheme.numberOfSharesForRecovery scheme, Is.EqualTo(7u))
+        Assert.That(ThresholdScheme.numberOfSharesToMake scheme, Is.EqualTo(9u))
 
     [<Test>]
     let ``Given invalid values, when creating a threshold scheme, then error is thrown`` () =
